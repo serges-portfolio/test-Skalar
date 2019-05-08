314dd3f7ccded0f970982157bf0b6a1706549b89
chaplygineverad <s.chaplygin@everad.com>
2019-05-08 16:27:44 +0300

    section 'smart' completed without adaptive

--- app/components/smart/smart.js
+++ app/components/smart/smart.js
@@ -2,7 +2,7 @@
 var showBox = document.querySelectorAll(".smart__filter-head");
 var rotateArrow = document.querySelectorAll(".smart__filter-arrow");
 
-for (i = 0; i < showBox.length; i++) {
+for ( let i = 0; i < showBox.length; i++) {
 
             showBox[i].addEventListener("click", function() {
             this.parentElement.classList.toggle("smart__opened-filter");
@@ -31,9 +31,27 @@ $( function() {
 //show-more filter
 var showCheckItem = document.querySelectorAll(".smart__more-button");
 
-for (i = 0; i < showCheckItem.length; i++) {
+for ( let i = 0; i < showCheckItem.length; i++) {
 
     showCheckItem[i].addEventListener("click", function() {
         this.parentElement.classList.toggle("-other-checkboxes-disable");
     });
 }
+
+//sort active
+
+var tabChange = document.querySelectorAll(".smart__sort-name");
+var sortOrientation = document.querySelectorAll(".smart__type");
+
+function tabToggle(elements, className) {
+    elements.forEach(function (tab) {
+        tab.addEventListener('click', function() {
+            const active = document.getElementsByClassName(className)[0];
+            active.classList.remove(className);
+            this.classList.add(className);
+        })
+    })
+}
+tabToggle(tabChange, 'smart__active-sort');
+tabToggle(sortOrientation, 'smart__type-image-active');
+
